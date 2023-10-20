defea87758e6af80aabe64f86c9b4ae4145f222b
anakhapavi <71835996+anakhapavi@users.noreply.github.com>
2023-10-20 12:55:56 +0530

    dropdown.js

---
+++ Reqruitement Mangement System for HR_MVC/js/dropdown.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#state-dropdown").select2({
+        placeholder: "-Select-",
+        allowClear: true,
+        width: "100%"
+    });
+
+    $("#city-dropdown").select2({
+        placeholder: "-Select-",
+        allowClear: true,
+        width: "100%"
+    });
+});
